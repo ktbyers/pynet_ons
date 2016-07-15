2f5e46f0b1e6e2067a3b70ea343788f2af231254
Kirk Byers <ktbyers@twb-tech.com>
2016-07-15 17:33:07 +0000

    day1; exercise1

---
+++ day1/ex1_strings.py
@@ -0,0 +1,11 @@
+name1 = "Kirk Byers"
+name2 = "George Washington"
+name3 = "Thomas Jefferson"
+name4 = raw_input("Enter fourth name: ")
+
+print
+print "{:>30}".format(name1)
+print "{:>30}".format(name2)
+print "{:>30}".format(name3)
+print "{:>30}".format(name4)
+print
